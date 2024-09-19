ca794f9012edff602f5f151a94ef2d994a15e63c
xiaoxinlee <2014402458@qq.com>
2024-09-19 10:35:25 +0800

    完成缓存配置，以及引入caffeine坐本地缓存

---
+++ framework/cache/cahce-core/src/main/java/com/xiaoxin/pan/core/constanst/CacheConstants.java
@@ -0,0 +1,13 @@
+package com.xiaoxin.pan.core.constanst;
+
+/**
+ * X Pan 服务端公用缓存常量类
+ */
+public interface CacheConstants {
+
+    /**
+     * X Pan 服务端公用缓存名称
+     */
+    String X_PAN_CACHE_NAME = "X_PAN_CACHE";
+
+}
